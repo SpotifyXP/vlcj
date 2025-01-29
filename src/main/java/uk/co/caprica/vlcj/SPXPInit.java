b0459dee9320c455b477968396616f3a93e35c90
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2025-01-29 14:27:49 +0100

    Add logging

--- src/main/java/uk/co/caprica/vlcj/SPXPInit.java
+++ src/main/java/uk/co/caprica/vlcj/SPXPInit.java
@@ -1,11 +1,21 @@
 package uk.co.caprica.vlcj;
 
+import com.sun.jna.NativeLibrary;
+
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 
 public class SPXPInit {
     public static void init() {
+        Class<?> logger = null;
         try {
+            try {
+                logger = Class.forName("com.spotifyxp.logging.ConsoleLoggingModules");
+            }catch (ClassNotFoundException ignored) {}
+            Class<?> publicValues = Class.forName("com.spotifyxp.PublicValues");
+            NativeLibrary.addSearchPath("libvlc", publicValues.getField("appLocation").get(null).toString() + File.separator + "vlc");
             Class<?> util = Class.forName("com.spotifyxp.PublicValues");
             Class<?> interfaceClass = Class.forName("com.spotifyxp.video.VLCPlayer");
             VideoPlayer player = new VideoPlayer();
@@ -22,7 +32,15 @@ public class SPXPInit {
             );
             util.getField("vlcPlayer").set(null, proxyInstance);
         } catch (Exception ex) {
-            throw new RuntimeException(ex);
+            if(logger != null) {
+                try {
+                    logger.getDeclaredMethod("Throwable", Throwable.class).invoke(logger, ex);
+                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                    ex.printStackTrace();
+                }
+            } else {
+                ex.printStackTrace();
+            }
         }
     }
 }
