9423364c84281f2a3a769a0d6cc0cce571c7f462
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2025-02-22 15:17:08 +0100

    Update VideoPlayer.java

--- src/main/java/uk/co/caprica/vlcj/VideoPlayer.java
+++ src/main/java/uk/co/caprica/vlcj/VideoPlayer.java
@@ -1,16 +1,49 @@
 package uk.co.caprica.vlcj;
 
-import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
-
+import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerEventListener;
+import uk.co.caprica.vlcj.player.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.videosurface.CanvasVideoSurface;
+import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("unused")
 public class VideoPlayer {
-    private final EmbeddedMediaPlayerComponent mediaPlayer;
+    private EmbeddedMediaPlayer mediaPlayer;
     private Runnable currentTakeover;
+    private CanvasVideoSurface canvasVideoSurface;
+    private boolean wasReleased = true;
+    private final MediaPlayerFactory mediaPlayerFactory;
+    private final Panel component;
+    private final Canvas canvas;
+    private Runnable onPlay;
 
     public VideoPlayer() {
-        mediaPlayer = new EmbeddedMediaPlayerComponent();
+        mediaPlayerFactory = new MediaPlayerFactory(
+                "--video-title=SpotifyXP video output",
+                "--no-snapshot-preview",
+                "--quiet-synchro",
+                "--sub-filter=logo:marq",
+                "--intf=dummy");
+        canvas = new Canvas();
+        canvas.setBackground(Color.BLACK);
+        component = new Panel();
+        component.setBackground(Color.BLACK);
+        component.setLayout(new BorderLayout());
+        component.add(canvas, BorderLayout.CENTER);
+        canvasVideoSurface = mediaPlayerFactory.newVideoSurface(canvas);
+    }
+
+    private void initializeVideoPlayback() {
+        mediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer(new FullScreenStrategy() {
+            @Override public void enterFullScreenMode() {}
+            @Override public void exitFullScreenMode() {}
+            @Override public boolean isFullScreenMode() { return false; }
+        });
+        mediaPlayer.setVideoSurface(canvasVideoSurface);
     }
 
     public boolean isVideoPlaybackEnabled() {
@@ -18,36 +51,56 @@ public class VideoPlayer {
     }
 
     public Panel getComponent() {
-        return mediaPlayer;
+        return component;
     }
 
     public void play(String uriOrFile) {
-        mediaPlayer.getMediaPlayer().playMedia(uriOrFile);
+        canvas.setVisible(true);
+        mediaPlayer.playMedia(uriOrFile);
     }
 
     public void pause() {
-        mediaPlayer.getMediaPlayer().pause();
+        mediaPlayer.pause();
     }
 
     public void stop() {
-        mediaPlayer.getMediaPlayer().stop();
+        canvas.setVisible(false);
+        mediaPlayer.stop();
+    }
+
+    public boolean wasReleased() {
+        return wasReleased;
+    }
+
+    public void release() {
+        mediaPlayer.release();
+        mediaPlayer = null;
+        wasReleased = true;
     }
 
     public void setLooping(boolean looping) {
-        mediaPlayer.getMediaPlayer().setRepeat(looping);
+        mediaPlayer.setRepeat(looping);
     }
 
     public boolean isLooping() {
-        return mediaPlayer.getMediaPlayer().getRepeat();
+        return mediaPlayer.getRepeat();
     }
 
     public boolean isPlaying() {
-        return mediaPlayer.getMediaPlayer().isPlaying();
+        return mediaPlayer.isPlaying();
     }
 
     public void init(Runnable onTakeover) {
-        stop();
+        if(wasReleased) {
+            initializeVideoPlayback();
+            wasReleased = false;
+        }
+        if(mediaPlayer.isPlaying()) mediaPlayer.stop();
         if(currentTakeover != null) currentTakeover.run();
         currentTakeover = onTakeover;
+    }
+
+    public void resume() {
+        mediaPlayer.play();
     }
 }
